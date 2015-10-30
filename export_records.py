ad9adf88a7ca1aa29bcaef5efc81f9bac0b3b13a
natashaa <natasha@dubizzle.com>
2015-10-30 12:52:03 +0530

    added export records

---
+++ export_records.py
@@ -0,0 +1,35 @@
+import csv
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rssb.settings'
+
+
+
+from sots.models import SOTSCallRecord
+
+file_name = '/home/natasha/Documents/records.csv'
+
+print file_name
+writer = csv.writer(open("temp.csv","wb"))
+
+records = SOTSCallRecord.objects.all()
+
+model = records.model
+    
+headers = []
+for field in model._meta.fields:
+    headers.append(field.name)
+writer.writerow(headers)
+    
+for obj in records:
+    row = []
+    for field in headers:
+        val = getattr(obj, field)
+        if callable(val):
+            val = val()
+        if type(val) == unicode:
+            val = val.encode("utf-8")
+        row.append(val)
+    writer.writerow(row)
+
+
+
